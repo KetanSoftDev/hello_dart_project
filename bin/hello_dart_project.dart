46a2ff4bb547cd4d329eddd9ff21fc1393247aa2
Ketan Shukla <ketansoftdev@gmail.com>
2021-12-20 01:43:40 -0800

    addition function and calls

--- bin/hello_dart_project.dart
+++ bin/hello_dart_project.dart
@@ -1,9 +1,13 @@
 void main() {
   print('Hello world!');
+  int tester = addition(5, 6);
+  double tester2 = doubleAddition(6, 7);
+  print(tester.toString() + ' ' + tester2.toString());
+  print(tester);
 }
 
-int addition() {
-  return 5;
+int addition(int x, int y) {
+  return x + y;
 }
 
 double doubleAddition(double x, double y) {
