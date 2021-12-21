059445c916d09dea4dad9fc4e418dd5b939c0dc9
Ketan Shukla <ketansoftdev@gmail.com>
2021-12-20 20:40:09 -0800

    added launch.json

--- bin/hello_dart_project.dart
+++ bin/hello_dart_project.dart
@@ -1,9 +1,23 @@
+import 'dart:math';
+
 void main() {
   print('Hello world!');
   int tester = addition(5, 6);
   double tester2 = doubleAddition(6, 7);
   print(tester.toString() + ' ' + tester2.toString());
   print(tester);
+  print(22.0 ~/ 7.0);
+  print(28 % 10);
+  print(siner());
+  print(coser());
+}
+
+double siner() {
+  return sin( 45 * pi / 180);
+}
+
+double coser() {
+  return cos(135 * pi / 180);
 }
 
 int addition(int x, int y) {
